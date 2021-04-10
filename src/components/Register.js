fed2b41de970d52366537acc4ee4e432cf0dae0a
Nidvar <djayshine87@hotmail.com>
2021-04-10 18:36:18 +1000

    register function

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,71 @@
 import React from 'react';
 
-const Register = (props)=>{
-    console.log(props)
-    return(
-        <div className="Signin">
-            <div>
-                Name: <input onChange={props.grab_value} /> <br />
-                <br />
-                Email: <input onChange={props.grab_value} /> <br />
-                <br />
-                Password: <input onChange={props.grab_value} /> <br />
-                <br />
-                <button onClick={()=>{props.onRoutechange('home')}} >Register</button>
+class Register extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            email:'',
+            password:'',
+            name:''
+        }
+    }
+    onEmailChange=(e)=>{
+        this.setState(()=>{
+            return {
+                email: e.target.value
+            }
+        })
+    }
+    onPasswordChange=(e)=>{
+        this.setState(()=>{
+            return {
+                password: e.target.value
+            }
+        })
+    }
+    onNameChange=(e)=>{
+        this.setState(()=>{
+            return {
+                name: e.target.value
+            }
+        })
+    }
+    onRegister=(e)=>{
+        fetch('http://localhost:3001/register', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id:4,
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
+            })
+        })
+        .then(a=>{
+            return a.json();
+        })
+        .then(a=>{
+            console.log(a)
+        })
+    }
+    render(){
+        return(
+            <div className="Signin">
+                <div>
+                    Name: <input onChange={this.onNameChange} /> <br />
+                    <br />
+                    Email: <input onChange={this.onEmailChange} /> <br />
+                    <br />
+                    Password: <input onChange={this.onPasswordChange} /> <br />
+                    <br />
+                    <button onClick={()=>{
+                        this.onRegister()
+                        this.props.onRoutechange('home')
+                    }} >Register</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Register
